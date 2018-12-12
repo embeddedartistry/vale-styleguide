007e2baba43d52283d6c70c7cc1e4247fb994139
Phillip Johnston <phillip@embeddedartistry.com>
2018-12-11 17:32:20 -0800

    Enable differentiation of documentation and blog scenario steps

--- features/blog_steps.rb
+++ features/blog_steps.rb
@@ -1,6 +1,6 @@
-cmd = 'vale --output=line --sort --normalize --relative'
+cmd = 'vale --config blog.vale.ini --output=line --sort --normalize --relative'
 
-When(/^I test "(.*)"$/) do |rule|
+When(/^Blog test "(.*)"$/) do |rule|
   step %(I cd to "../../fixtures/#{rule}")
   step %(I run `#{cmd} .`)
 end
--- features/documentation_steps.rb
+++ features/documentation_steps.rb
@@ -1,6 +1,6 @@
-cmd = 'vale --output=line --sort --normalize --relative'
+cmd = 'vale --config documentation.vale.ini --output=line --sort --normalize --relative'
 
-When(/^I test "(.*)"$/) do |rule|
+When(/^Doc test "(.*)"$/) do |rule|
   step %(I cd to "../../fixtures/#{rule}")
   step %(I run `#{cmd} .`)
 end
